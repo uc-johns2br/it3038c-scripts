155f0ae404dca55b6b67cb49026b42edad73216b
uc-johns2br <johns2br@mail.uc.edu>
2020-02-04 19:06:31 -0500

    adding powershell files

---
+++ Powershell/sysinfo.ps1
@@ -0,0 +1,21 @@
+﻿function getIP{
+    (Get-NetIPAddress).IPAddress | Select-String "192"
+}
+
+function getDate{
+    Get-Date -UFormat "%A, %B %d, %Y"  
+}
+
+
+$IP = getIP
+$USER = $env:USERNAME
+$HOSTNAME = $env:USERDOMAIN
+$VERSION = $Host.Version.Major
+$DATE = getDate
+
+
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell Version $VERSION. Today's Date is $DATE."
+
+Send-MailMessage -To "brettjohnson791@gmail.com" -from "me@mail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
+
+write-host("Email sent.")
