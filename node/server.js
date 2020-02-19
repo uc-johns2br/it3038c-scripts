79e21376d84c5bd7e7fc1cf10a09efa35040786e
uc-johns2br <johns2br@mail.uc.edu>
2020-02-18 19:24:58 -0500

    too much to say

---
+++ node/server.js
@@ -0,0 +1,33 @@
+var http = require("http");
+var fs = require("fs");
+var os = require("os");
+var ip = require("ip");
+
+
+http.createServer(function(req, res){
+    
+    if(req.url === "/"){
+        fs.readFile("./public/index.html", "UTF-8", function(err, body)){
+        res.writeHead(200,{"Content-Type": "text/html"});
+        res.end(body);
+        });
+    }
+}
+    else if(req.url.match("/sysinfo")){
+        myHostname=os.hostname();
+        html=`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>System Info</title>
+            </head>
+            <body>
+                <p>
+        `
+    }
+    
+
+
+server.listen(3000);
+
+console.log("Server listening on port 3000");
