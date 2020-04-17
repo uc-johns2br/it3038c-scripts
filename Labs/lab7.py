8e9ef78a7a632ded10c43769f9fa4f3309af21da
uc-johns2br <johns2br@mail.uc.edu>
2020-04-17 05:02:48 -0400

    finished lab 7 code

--- Labs/lab7.py
+++ Labs/lab7.py
@@ -1,7 +1,28 @@
-#This script uses the docx2python module
+"""This script uses the docx2python module, which extracts .docx headers,
+footers, text, footnotes, endnotes, properties, and images to a Python object."""
 
 from docx2python import docx2python
 
-#Ask for input
-print ('Please enter a file path to a .docx (leave blank and press enter for default of "C:\Users\Administrator\Documents\Lab7.docx):"')
-word = input()
+#Show file properties
+print ("Properties:")
+print (docx2python('./Lab7.docx').properties)
+print ('\n')
+
+print ("Press enter to continue")
+keypress = input()
+print ('\n')
+
+#Show Header
+print ("Header:")
+print (docx2python('./Lab7.docx').header)
+print ('\n')
+
+print ("Press enter to continue")
+keypress = input()
+print ('\n')
+
+#Show Footer
+print ("Footer:")
+print (docx2python('./Lab7.docx').footer)
+print ('\n')
+
