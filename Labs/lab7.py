3fa85d703a03845ef9cd92443070c86240aef434
uc-johns2br <johns2br@mail.uc.edu>
2020-04-17 03:59:02 -0400

    created lab7

---
+++ Labs/lab7.py
@@ -0,0 +1,7 @@
+#This script uses the docx2python module
+
+from docx2python import docx2python
+
+#Ask for input
+print ('Please enter a file path to a .docx (leave blank and press enter for default of "C:\Users\Administrator\Documents\Lab7.docx):"')
+word = input()
