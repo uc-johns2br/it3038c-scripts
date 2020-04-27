8bd516aa383d1d1fd31ba41ed3314cb03b687ff6
uc-johns2br <johns2br@mail.uc.edu>
2020-04-27 06:53:42 -0400

    commit changes t

--- python/routes.py
+++ python/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -6,9 +6,9 @@ app.config.from_object(__name__)
 
 @app.route("/")
 def hello():
-    name = 'AJ'
-    return render_template("index.html", value=name)
+    #name = 'AJ'
+    return render_template("index.html")
 
-@app.route("/welcome")
+@app.route("/welcome", methods=['POST'])
 def welcome():
-    return render_template("welcome.html")
+    return render_template("welcome.html", myName=request.form['myName'])
